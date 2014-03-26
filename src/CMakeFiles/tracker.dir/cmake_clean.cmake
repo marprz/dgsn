bf399248dfbcac7c4084eca7706c05fd1f0020ad
Marta Przyszlak <marta.marprz@gmail.com>
2014-03-26 13:35:34 +0100

    Added CMake files

---
+++ src/CMakeFiles/tracker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tracker.dir/GaussianMatrix.cpp.o"
+  "CMakeFiles/tracker.dir/main.cpp.o"
+  "../build/tracker.pdb"
+  "../build/tracker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
